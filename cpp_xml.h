306c104cc2eb9a3baf030613f27b2d6efed6215f
william <wharrold90@gmail.com>
2021-06-22 20:04:42 +0000

    added xmlDocWrite function and turned all member functions to constexpr

--- cpp_xml.h
+++ cpp_xml.h
@@ -11,10 +11,22 @@
 #include<iterator>
 #include<streambuf>
 
+struct XMLNode;
+
+
+struct XMLDocument
+{
+  XMLNode* root;
+  std::string version;
+  std::string encoding;
+};
+
 
 class CPP_XML
 {
   private:
+
+
     struct XMLAttribute
     {
       std::string key;
@@ -30,15 +42,16 @@ class CPP_XML
       std::vector<XMLNode*> children;
     };
 
-    struct XMLDocument
-    {
-      XMLNode* root;
-      std::string version;
-      std::string encoding;
-    };
+    //member variables
+    private:
+    XMLDocument m_doc;
+    
+
+
+
 
   private:
-    std::vector<XMLNode*> XMLNodeChildren (XMLNode* parent, const std::string & tag)
+  constexpr  std::vector<XMLNode*> XMLNodeChildren (XMLNode* parent, const std::string & tag)
     {
       std::vector<XMLNode*> list:
 
@@ -78,7 +91,7 @@ class CPP_XML
       TAG_INLINE
     };
 
-    TagType parseAttrs(std::string& buf, std::string::size_type* index,std::string& lex, std::string::size_type* lexi, XMLNode* curr_node)
+   constexpr TagType parseAttrs(std::string& buf, std::string::size_type* index,std::string& lex, std::string::size_type* lexi, XMLNode* curr_node)
     {
       XMLAttribute curr_attr;
       while(buf[*index] != '>'){
@@ -109,7 +122,7 @@ class CPP_XML
         //Attribute value
         if(buf[*index] == '"'){
           if(!curr_attr.key){
-            std::cerr << "Value has no key" << std::endl;
+            std::cerr << "Value has no key" << '\n';
             return TAG_START;
           }
           *lexi =0;
@@ -141,7 +154,7 @@ class CPP_XML
       return TAG_START;
     }
 
-    void nodeOut(std::ofstream& file, XMLNode* node, int indent, int times)
+    constexpr void nodeOut(std::ofstream& file, XMLNode* node, int indent, int times)
     {
       for(decltype(node->children.size()) index = 0; index < node->children.size(); ++index){
         XMLNode* child = node->children[index];
@@ -159,17 +172,17 @@ class CPP_XML
           file << ' ' << attr.key << '\"' << attr.value << '\"';
         }
         if(child->children.size() == 0 && !child->inner_text)
-          file << "/>" << std::endl;
+          file << "/>" << '\n';
         else{
           file << '>';
           if(child->children.size() == 0)
-            file << child->inner_text << "</" << child->tag << '>' << endl;
+            file << child->inner_text << "</" << child->tag << '>' << '\n';
           else{
-            file << endl;
+            file << '\n';
             nodeOut(file, child, indent, times +1);
             if(times > 0)
               file << std::string(indent*times, " ");
-            file << "</" << child->tag << '>' << endl;
+            file << "</" << child->tag << '>' << '\n';
           }
 
         }
@@ -179,11 +192,11 @@ class CPP_XML
 
     //Public function
   public:
-    bool XMLDocLoad(XMLDocument* doc, const std::string& path)
+    constexpr bool XMLDocLoad(XMLDocument* doc, const std::string& path)
     {
       std::ifstream file (path);
       if(!file.is_ooen()){
-        std::cerr << "Could not load file from " << path << std::endl;
+        std::cerr << "Could not load file from " << path << '\n';
         return false;
       }
       std::string buf;
@@ -214,7 +227,7 @@ class CPP_XML
           //inner text
           if(lexi > 0){
             if(!curr_node){
-              std::cerr << "Text outside of document" << std::endl;
+              std::cerr << "Text outside of document" << '\n';
               return false;
             }
             curr_node->inner_text = lex;
@@ -230,12 +243,12 @@ class CPP_XML
             }
 
             if(!curr_node){
-              std::cerr << "Already at root node" << std::endl;
+              std::cerr << "Already at root node" << ''\n';
               return false;
             }
 
             if(curr_node->tag.compare(lex) != 0){
-              std::cerr << "Mismatched tags (" << curr_node->tag << "!=" << lex << ")" << std::endl;
+              std::cerr << "Mismatched tags (" << curr_node->tag << "!=" << lex << ")" << '\n';
               return false;
             }
             curr_node = curr_node->parent;
@@ -254,7 +267,7 @@ class CPP_XML
             if(lex.compare("<!--") == 0){
               //pre c++20 compiler therefore i must 
               //implement my own ends_wth function
-              const auto ends_wth = [](const std::string& str, const std::string& suffix){
+              constexpr auto ends_wth = [](const std::string& str, const std::string& suffix){
                 return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix);
               }
               while(!ends_wth(lex, "-->")){
@@ -317,8 +330,25 @@ class CPP_XML
     return true;
     }
 
-   
-   
+   constexpr void XMLDocWrite (std::ostream& os, XMLDocument* doc, const std::string& path, int indent)
+    {
+      os << "<?xml version \"" << (doc->version) ? doc->version : "1.0" << "encoding \"" << (doc->encoding) ? doc->encoding : "U5F-8" << "\" ?>\n";
+
+      nodeOut(os, doc->root, indent, 0);
+      std::flush;
+    }
+
+  public:
+    CPP_XML() = default;
+    CPP_XML(XMLDocument doc, const std::string& path): m_doc(doc)
+    {
+      if(!XMLDocLoad(m_doc, m_path))
+      {
+        std::cout << "Cannot load file: " << path <<'\n';
+      }
+      
+    }
+
 };
 
 #endif//CPP_XML_H
